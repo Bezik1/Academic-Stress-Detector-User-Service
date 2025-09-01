b29f9a948ac313b13c96b2f1294b82f1e71da05e
Mateusz <mateusz.adamowicz.lo1@gmail.com>
2025-09-01 20:53:43 +0200

    Services v2

--- src/main/java/com/userservice/userservice/service/PostService.java
+++ src/main/java/com/userservice/userservice/service/PostService.java
@@ -2,29 +2,43 @@ package com.userservice.userservice.service;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
-import com.userservice.userservice.repository.PostRepository;
-import com.userservice.userservice.model.Post;
+import com.userservice.userservice.repository.SessionRepository;
+import com.userservice.userservice.model.Session;
 
 
 @Service
 public class PostService {
-    private final PostRepository postRepository;
+    private final SessionRepository sessionRepository;
 
-    public PostService(PostRepository postRepository) {
-        this.postRepository = postRepository;
+    public PostService(SessionRepository sessionRepository) {
+        this.sessionRepository = sessionRepository;
     }
 
-    public List<Post> getAllPosts() {
-        return postRepository.findAll();
+    public List<Session> getAllSessions() {
+        return sessionRepository.findAll();
     }
 
-    public Post addPost(Post post) {
-        return postRepository.save(post);
+    public Session addSession(Session post) {
+        return sessionRepository.save(post);
     }
 
-    public List<Post> getPostsByUser(Long userId) {
-        return postRepository.findByUserId(userId);
+    public List<Session> getSessionsByUserId(Long userId) {
+        return sessionRepository.findByUserId(userId);
+    }
+
+    public Session updateSession(Long sessionId, Session newSession) {
+        Session existingSession = sessionRepository.findById(sessionId)
+                .orElseThrow(() -> new RuntimeException("Session not found with id " + sessionId));
+
+        BeanUtils.copyProperties(newSession, existingSession, "id");
+
+        return sessionRepository.save(existingSession);
+    }
+
+    public void removeSessionById(Long sessionId) {
+        sessionRepository.deleteById(sessionId);
     }
 }
