ddc43afd98dc68dcdd5dd4025cb4966fd4350a85
Mateusz <mateusz.adamowicz.lo1@gmail.com>
2025-09-01 20:58:08 +0200

    Controllers v2

--- src/main/java/com/userservice/userservice/controller/PostController.java
+++ src/main/java/com/userservice/userservice/controller/PostController.java
@@ -2,37 +2,49 @@ package com.userservice.userservice.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.userservice.userservice.service.PostService;
-import com.userservice.userservice.model.Post;
+import com.userservice.userservice.model.Session;
+import com.userservice.userservice.service.SessionService;
 
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
-    private final PostService postService;
+    private final SessionService sessionService;
 
-    public PostController(PostService postService) {
-        this.postService = postService;
+    public PostController(SessionService postService) {
+        this.sessionService = postService;
     }
 
     @GetMapping
-    public List<Post> getAllPosts() {
-        return postService.getAllPosts();
+    public List<Session> getAllSessions() {
+        return sessionService.getAllSessions();
     }
 
     @PostMapping
-    public Post addPost(@RequestBody Post post) {
-        return postService.addPost(post);
+    public Session addSession(@RequestBody Session post) {
+        return sessionService.addSession(post);
     }
 
     @GetMapping("/user/{userId}")
-    public List<Post> getPostsByUser(@PathVariable Long userId) {
-        return postService.getPostsByUser(userId);
+    public List<Session> getSessionsByUser(@PathVariable Long userId) {
+        return sessionService.getSessionsByUserId(userId);
+    }
+
+    @PutMapping("/user/{userId}")
+    public Session updateSession(@PathVariable Long sessionId, @RequestBody Session session) {
+        return sessionService.updateSession(sessionId, session);
+    }
+
+    @DeleteMapping("{sessionId}")
+    public void removeSession(@PathVariable Long sessionId) {
+        sessionService.removeSessionById(sessionId);
     }
 }
