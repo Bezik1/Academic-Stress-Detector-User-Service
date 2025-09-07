d62d66a7d75ae90c9aba2273a439a2a20c3732a9
Mateusz <mateusz.adamowicz.lo1@gmail.com>
2025-09-07 18:58:35 +0200

    Debugging

--- src/main/java/com/userservice/userservice/config/SecurityConfig.java
+++ src/main/java/com/userservice/userservice/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://academic-stress-detector-client-ser.vercel.app"));
+        configuration.setAllowedOrigins(List.of("https://academic-stress-detector-client-ser.vercel.app, http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
