7a28148be8706d1d134b8f0b436c53296994de75
Mateusz <mateusz.adamowicz.lo1@gmail.com>
2025-09-08 16:31:09 +0200

    Final error handling

--- src/main/java/com/userservice/userservice/config/SecurityConfig.java
+++ src/main/java/com/userservice/userservice/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://academic-stress-detector-client-ser.vercel.app", "http://localhost:5173", "https://academic-stress-detector-auth-service.onrender.com"));
+        configuration.setAllowedOrigins(List.of("https://academic-stress-detector-client-ser.vercel.app", "https://academic-stress-detector-auth-service.onrender.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
